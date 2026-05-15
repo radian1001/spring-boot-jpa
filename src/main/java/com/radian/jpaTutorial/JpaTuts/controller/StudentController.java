afd2e67719c73dffaf0498c118cebf9fbfbcb851
radian1001 <baghelabhyudaya@gmail.com>
2026-05-15 10:18:18 +0530

    changes include modelmapper

--- src/main/java/com/radian/jpaTutorial/JpaTuts/controller/StudentController.java
+++ src/main/java/com/radian/jpaTutorial/JpaTuts/controller/StudentController.java
@@ -1,52 +1,56 @@
-//package com.radian.jpaTutorial.JpaTuts.controller;
-//
-//import com.radian.jpaTutorial.JpaTuts.dto.StudentDto;
-//import com.radian.jpaTutorial.JpaTuts.service.StudentService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequiredArgsConstructor
-//public class StudentController {
-//
-//    private final StudentService studentService;
-//
-//    @GetMapping("/students")
-//    public List<StudentDto> getAllStudents() {
-//        return studentService.getAllStudents();
-//    }
-//
-//    @GetMapping("/students/{id}")
-//    public StudentDto getStudentById(@PathVariable Long id) {
-//        return studentService.getStudentById(id);
-//    }
-//
-//    @PutMapping("/students/{id}")
-//    public StudentDto updateStudent(@PathVariable Long id, StudentDto studentDto) {
-//        return studentService.updateStudent(id, studentDto);
-//    }
-//
-//    @PostMapping("/students/{studentId}/subjects/{subjectId}")
-//    public StudentDto assignStudentToCourse(@PathVariable Long studentId, @PathVariable Long courseId) {
-//        return studentService.assignStudentToCourse(studentId, courseId);
-//    }
-//
-//    @PostMapping("/students/{studentId}/professors/{professorId}")
-//    public StudentDto assignStudentToProfessor(@PathVariable Long studentId, @PathVariable Long professorId) {
-//        return studentService.assignStudentToProfessor(studentId, professorId);
-//    }
-//
-//    @DeleteMapping("/students/{studentId}/subjects/{subjectId}")
-//    public StudentDto removeStudentFromCourse(@PathVariable Long studentId, @PathVariable Long courseId) {
-//        return studentService.removeStudentFromCourse(studentId, courseId);
-//    }
-//
-//    @DeleteMapping("/students/{studentId}/professors/{professorId}")
-//    public StudentDto removeStudentFromProfessor(@PathVariable Long studentId, @PathVariable Long professorId) {
-//        return studentService.removeStudentFromProfessor(studentId, professorId);
-//    }
-//
-//
-//}
+package com.radian.jpaTutorial.JpaTuts.controller;
+
+import com.radian.jpaTutorial.JpaTuts.dto.StudentDto;
+import com.radian.jpaTutorial.JpaTuts.service.StudentService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController {
+
+   private final StudentService studentService;
+
+   @GetMapping("/students")
+   public Page<StudentDto> getAllStudents(@RequestParam(defaultValue = "0") int pageNumber,
+                                         @RequestParam(defaultValue = "5") int pageSize,
+                                         @RequestParam(defaultValue = "id") String sortBy) {
+       return studentService.getAllStudents(pageNumber, pageSize, sortBy);
+   }
+
+   @GetMapping("/students/{id}")
+   public StudentDto getStudentById(@PathVariable Long id) {
+       return studentService.getStudentById(id);
+   }
+
+   @PutMapping("/students/{id}")
+   public StudentDto updateStudent(@PathVariable Long id, StudentDto studentDto) {
+       return studentService.updateStudent(id, studentDto);
+   }
+
+   @PostMapping("/students/{studentId}/subjects/{subjectId}")
+   public StudentDto assignStudentToCourse(@PathVariable Long studentId, @PathVariable Long courseId) {
+       return studentService.assignStudentToCourse(studentId, courseId);
+   }
+
+   @PostMapping("/students/{studentId}/professors/{professorId}")
+   public StudentDto assignStudentToProfessor(@PathVariable Long studentId, @PathVariable Long professorId) {
+       return studentService.assignStudentToProfessor(studentId, professorId);
+   }
+
+   @DeleteMapping("/students/{studentId}/subjects/{subjectId}")
+   public StudentDto removeStudentFromCourse(@PathVariable Long studentId, @PathVariable Long courseId) {
+       return studentService.removeStudentFromCourse(studentId, courseId);
+   }
+
+   @DeleteMapping("/students/{studentId}/professors/{professorId}")
+   public StudentDto removeStudentFromProfessor(@PathVariable Long studentId, @PathVariable Long professorId) {
+       return studentService.removeStudentFromProfessor(studentId, professorId);
+   }
+
+
+}
--- src/main/java/com/radian/jpaTutorial/JpaTuts/service/StudentService.java
+++ src/main/java/com/radian/jpaTutorial/JpaTuts/service/StudentService.java
@@ -1,21 +1,35 @@
 package com.radian.jpaTutorial.JpaTuts.service;
 
+import com.radian.jpaTutorial.JpaTuts.dto.StudentDto;
 import com.radian.jpaTutorial.JpaTuts.entity.Student;
 import com.radian.jpaTutorial.JpaTuts.repository.StudentRepo;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class StudentService {
 
     private final StudentRepo studentRepository;
+    private final ModelMapper modelMapper;
 
-    public StudentService(StudentRepo studentRepository) {
+    public StudentService(StudentRepo studentRepository, ModelMapper modelMapper) {
         this.studentRepository = studentRepository;
+        this.modelMapper = modelMapper;
     }
 
-    public List<Student> getAllStudents() {
-        return studentRepository.findAll();
+    public Page<StudentDto> getAllStudents(int pageNumber, int pageSize, String sortBy) {
+        Sort sort = Sort.by(new Sort.Order(Sort.Direction.ASC, sortBy));
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+        
+        return studentRepository.findAll(pageable)
+                .map(student -> modelMapper.map(student, StudentDto.class));
     }
 }
